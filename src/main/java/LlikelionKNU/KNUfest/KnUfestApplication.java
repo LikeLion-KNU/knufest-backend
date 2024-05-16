7f875d8aca79447e79820af6698767e77eac16ed
DongGeon Lee <71763322+himodu@users.noreply.github.com>
2024-05-17 03:30:02 +0900

    [MODIFY] Set TimeZone to Asia/Seoul
    - Set TimeZone to Asia/Seoul

--- src/main/java/LlikelionKNU/KNUfest/KnUfestApplication.java
+++ src/main/java/LlikelionKNU/KNUfest/KnUfestApplication.java
@@ -1,15 +1,20 @@
 package LlikelionKNU.KNUfest;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @EnableJpaAuditing
 @SpringBootApplication
 public class KnUfestApplication {
-
+	@PostConstruct
+	void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(KnUfestApplication.class, args);
 	}
-
 }
